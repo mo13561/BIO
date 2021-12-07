ac3a4293c6616917b6c9d1407d40631eb7276508
mo13561 <91287581+mo13561@users.noreply.github.com>
2021-12-07 23:18:44 +0000

    379A - New Year Candles

---
+++ codeforces/Candles.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int a; int b;
+	cin >> a >> b;
+	int c = 0;
+	int ans = 0;
+	ans += a;
+	c += a;
+	a = 0;
+	while (true) {
+		a += (c - (c % b)) / b;
+		c -= (c - (c % b));
+		ans += a;
+		c += a;
+		a = 0;
+		if (c < b) {
+			break;
+		}
+	}
+	cout << ans;
